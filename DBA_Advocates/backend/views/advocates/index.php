83dd26b1d34dd0d0d5b5d840b43a1e8b5cd28116
Nadia Gull <nadiagull285@gmail.com>
2018-03-27 13:41:57 -0700

    update view page

--- DBA_Advocates/backend/views/advocates/index.php
+++ DBA_Advocates/backend/views/advocates/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\AdvocatesSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -20,41 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Advocates', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php  
-        $gridColumns = [
-            //'adv_id',
-            'adv_Name',
-            'adv_Father_Name',
-            'adv_CNIC',
-            'adv_Phone_No',
-            'adv_Address',
-            'adv_SNo',
-            'adv_BRPNo',
-            'adv_HCRNo',
-            'adv_EDSC',
-            'adv_EDHC',
-            'adv_EDLC',
-            'adv_VMSC',
-            'adv_VMHC',
-            'adv_VMLC',
-            'adv_NICNo',
-            //'adv_Photo',
-            //'adv_Image_CNIC',
-            //'adv_Image_License',
-            'adv_Voting_Eligibility',
-            //'adv_Status',
-            //'adv_Created_At',
-            //'adv_Created_By',
-            //'adv_Updated_At',
-            //'adv_Updated_By',
-        ];
-
-        //Reader a export dropdown menu
-        echo ExportMenu::widget([
-            'dataProvider' => $dataProvider,
-            'columns' => $gridColumns
-        ]);
-    ?>
+    
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
